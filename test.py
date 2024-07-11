838353f3370d1a206c613d6e666fcf6e03cc5f73
Nischay Purnekar <97733876+NischayPurnekar@users.noreply.github.com>
2024-07-11 14:44:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-
 import torch
 from torchvision import transforms
 from torchvision.transforms import ToTensor
 import networks
 
 
+   
+    """" The code translates the images from digital domain to the P&S domain (vice-versa).
+    The CycleGAN model architecture is loaded using the networks script.
+    Cycle GAN simulators are loaded resulting in the translation of images.
+    
+    """
+    
 def tensor2im(input_image, imtype=np.uint8):
     """"Converts a Tensor array into a numpy image array.
 
@@ -26,6 +32,13 @@ def tensor2im(input_image, imtype=np.uint8):
     return image_numpy.astype(imtype)
 
 def cyclegan(input_image):
+
+    """"Image translation using trained CycleGAN simulators.
+
+    Parameters:
+        input_image (array) --  the input image array
+    """
+    #resize the input image to (256,256,3)
     input_image = cv2.resize(input_image, (256, 256), interpolation=cv2.INTER_CUBIC)
     # Convert BGR to RGB (OpenCV loads images in BGR by default)
     input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
@@ -36,26 +49,22 @@ def cyclegan(input_image):
     ])
     input_tensor = transform(input_image)
     input_tensor = input_tensor.unsqueeze(0)
-    #input_tensor = ToTensor()(input_image).unsqueeze(0)# Generate an image using the generator
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(device)
-    netG_A = networks.define_G(3, 3, 64, 'unet_256', 'instance',
+    
+    input_nc = 3, 
+    output_nc = 3,
+    ngf = 64
+    netG = 'unet_256'
+    
+    netG_A = networks.define_G(input_nc, output_nc, ngf, netG, 'instance',
                                not 'store_true', 'normal', 0.02, '0')
 
+    #Load the trained Simulator
     model_path = 'latest_net_G_A.pth'
     netG_A.load_state_dict(torch.load(model_path, map_location=device))
     with torch.no_grad():
-        #generated_image = netG_A(input_tensor.to(device))
         generated_image = netG_A(input_tensor)
-
     # Convert the generated image tensor to a NumPy array
-    #generated_image = generated_image.squeeze(0).cpu().numpy()
-    # Define the mean and standard deviation used during normalization
     generated_image = tensor2im(generated_image)
-    #generated_image = generated_image.squeeze(0).cpu().numpy()
-    # Convert from RGB to BGR (OpenCV saves images in BGR format)
-    #generated_image = generated_image.transpose((1, 2, 0))
     generated_image = cv2.cvtColor(generated_image, cv2.COLOR_RGB2BGR)
-    #generated_image = generated_image * 255
     return generated_image
-
